cfdd1b4f17ed083f73d96ce270d32bdd574ed9df
westbot <82739021+westbot657@users.noreply.github.com>
2024-09-05 15:58:19 -0600

    testing more stuff

--- docs/assets/js/project_fetcher.js
+++ docs/assets/js/project_fetcher.js
@@ -1,34 +1,56 @@
 
 const project_data = {
     "Insert Dungeon Name Here": {
-        "url": "https://raw.githubusercontent.com/westbot657/DungeonEngine/main/project_meta.json"
+        "url": "https://raw.githubusercontent.com/westbot657/DungeonEngine/main/project_meta.json",
+        "background": "assets/content/dungeon_engine_bg.png"
     },
     "Ethereal Enchanting": {
-        "url": "https://raw.githubusercontent.com/westbot657/EtherealEnchanting/main/project_meta.json"
+        "url": "https://raw.githubusercontent.com/westbot657/EtherealEnchanting/main/project_meta.json",
+        "background": ""
     },
     "UI Library": {
-        "url": "https://raw.githubusercontent.com/westbot657/UILibrary/main/project_meta.json"
+        "url": "https://raw.githubusercontent.com/westbot657/UILibrary/main/project_meta.json",
+        "background": ""
     },
     "Blockbench TitleGenExtra": {
-        "url": "https://raw.githubusercontent.com/westbot657/BlockBenchTitleGenExtra/main/project_meta.json"
+        "url": "https://raw.githubusercontent.com/westbot657/BlockBenchTitleGenExtra/main/project_meta.json",
+        "background": ""
     }
 }
 
+function main() {
 
-class Project {
-    constructor(title, description, languages, version) {
-        this.title = title;
-        this.description = description
-        this.languages = languages
-        this.version = version
-    }
+    let section = document.getElementById("projects_section");
+
+    (async() => {
+        for (const [key, val] of Object.entries(project_data)) {
+            try {
+                let meta = await axios.get(val.url);
+                
+                let card =
+                "<div class='project-card' style='background-image:url(`"+ val.background +"`)'>\
+                <h1>"+ key +"</h1>\
+                <p>"+ meta.data.description +"</p>\
+                <p class='project-version'>"+ meta.data.version +"</p>\
+                </div>";
+
+                section.innerHTML += card;
+
+            } catch {
+                // maybe make a "failed to load" card
+                let card =
+                "<div class='project-card'>\
+                <h1>"+ key +"</h1>\
+                <p>Failed to load project data</p>\
+                </div>";
+
+                section.innerHTML += card;
+            }
+            
+        }
+    })()
 }
 
 
 
-(async() => {
-    for (const [key, val] of Object.entries(project_data)) {
-        const meta = await axios.get(val.url);
-        
-    }
-})()
+main();
